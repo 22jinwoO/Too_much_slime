e9b977a8726f9d5d2399357c9ca4d173855c668e
22jinWoO <jinwoo2511@naver.com>
2024-09-13 02:54:20 +0900

    feat : 발사체 기능 구현
    발사체 기능 구현

---
+++ Too_Much_Slime/Assets/1.Scripts/Projectile/Projectile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public float attackDmg;
+    public float MoveSpeed;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //rigid.AddForce(transform.up * MoveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        if(gameObject.CompareTag("PlayerBullet")) rigid.velocity = Vector2.up * MoveSpeed * Time.deltaTime;
+
+        //Debug.LogWarning(rigid.velocity);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.CompareTag("PlayerBullet") && collision.gameObject.CompareTag("Monster"))
+        {
+            if(collision!=null) collision.gameObject.GetComponent<UnitDamaged>().Damaged(attackDmg);
+
+            Destroy(gameObject);
+        }
+        
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (gameObject.CompareTag("PlayerBullet") && collision.gameObject.CompareTag("Monster")) collision.gameObject.GetComponent<UnitDamaged>().Damaged(attackDmg);
+
+    //}
+}
