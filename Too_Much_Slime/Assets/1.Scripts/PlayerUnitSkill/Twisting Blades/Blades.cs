416a8f98436cca5a99332038ae7995d5a8e3e24f
22jinWoO <jinwoo2511@naver.com>
2024-09-13 02:57:05 +0900

    feat : 회전 칼날 기능 구현 및 프리팹 생성
    회전 칼날 기능 구현 및 프리팹 생성

---
+++ Too_Much_Slime/Assets/1.Scripts/PlayerUnitSkill/Twisting Blades/Blades.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blades : MonoBehaviour
+{
+    [SerializeField]private float attackDmg;
+    
+    public SkillBase twistingBlades;
+
+    [SerializeField] private float rotateSpeed;
+
+    private void Awake()
+    {
+        rotateSpeed = 30f;
+    }
+    public void InitData()
+    {
+        // 칼날 데미지
+        attackDmg = twistingBlades.skillDamage;
+    }
+    private void Update()
+    {
+        // Z축을 기준으로 일정하게 회전시키기 (20도씩 회전)
+        transform.Rotate(0, 0, 20f * Time.deltaTime * rotateSpeed);
+    }
+    public void MoveTo(Vector2 position)
+    {
+        transform.position = position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(gameObject.CompareTag("PlayerBullet"));
+        print(collision.gameObject.CompareTag("Monster"));
+        if (gameObject.CompareTag("PlayerBullet") && collision.gameObject.CompareTag("Monster"))
+        {
+            if (collision != null) collision.gameObject.GetComponent<UnitDamaged>().Damaged(attackDmg);
+            print("몬스터와 충돌");
+        }
+    }
+}
