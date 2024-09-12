c009e13d33fe01d63ff5f64dcec4c8aff0a08f50
22jinWoO <jinwoo2511@naver.com>
2024-09-12 17:13:04 +0900

    feat : 플레이어 유닛과 몬스터 공통 스탯 클래스 구현
    플레이어 유닛과 몬스터 공통 스탯 클래스 구현

---
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/BaseUnitStats/BaseUnitStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUnitStats : MonoBehaviour
+{
+    public float maxHp;
+    public float curHp;
+    public float atkDmg;
+    public float atkSpd;
+}
