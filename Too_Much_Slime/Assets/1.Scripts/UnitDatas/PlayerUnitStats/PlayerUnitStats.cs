2f4215dea5109266a68eb8624ca88144bdf89443
22jinWoO <jinwoo2511@naver.com>
2024-09-13 00:03:10 +0900

    feat : 몬스터 스폰을 위해 플레이어의 최고 높이값 가져오는 기능 구현
    몬스터 스폰을 위해 플레이어의 최고 높이값 가져오는 기능 구현

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/PlayerUnitStats/PlayerUnitStats.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/PlayerUnitStats/PlayerUnitStats.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PlayerUnitStats : BaseUnitStats
-{
+{   
+    //플레이어 최고 높이값
+    private float maxY
+    {
+        get { return (transform.position.y); }
+    } 
+
+    // 읽기 전용으로만 쓸 때, 이 값을 불러와도 값에 변화를 주지 못하게 읽기로만 
+    public float MaxY
+    { get { return maxY; } }
+
     public float plusAtkDmg;
 
     public float plusAtkSpd;
@@ -11,6 +21,8 @@ public abstract class PlayerUnitStats : BaseUnitStats
     public float plusMaxHp;
 
     public float moveSpeed;
+
+    public SkillBase followeBirds;
 
     public abstract void InitData();
 }
