6da18dd7a850545b7cef7e4b34e824eee7d7070f
22jinWoO <jinwoo2511@naver.com>
2024-09-12 20:46:40 +0900

    fix : 조이스틱 레버 거리 영향 주는 기능 delete
    조이스틱 레버 거리 영향 주는 기능 delete

--- Too_Much_Slime/Assets/1.Scripts/JoyStick/JoyStick.cs
+++ Too_Much_Slime/Assets/1.Scripts/JoyStick/JoyStick.cs
@@ -21,7 +21,7 @@ public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPoint
     [SerializeField] bool isTouch = false;
 
     // 플레이어에게 이동 신호를 보낼 delegate
-    public Action<Vector2, float> OnMoveInput;  // 이동 방향, 조이스틱 레버의 거리(0~1)
+    public Action<Vector2> OnMoveInput;  // 이동 방향
 
     private void Awake()
     {
@@ -52,7 +52,7 @@ public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPoint
         value = Vector2.zero;
 
         // 이동 신호 전달 (방향 0, 거리 0)
-        OnMoveInput?.Invoke(value, value.magnitude);
+        OnMoveInput?.Invoke(value);
 
         rect_BackGround.gameObject.SetActive(false);
     }
@@ -70,14 +70,11 @@ public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPoint
             // 조이스틱 자식 위치를 value 값으로 지정
             rect_JoyStick.localPosition = value;
 
-            // 조이스틱 거리에 따라 이동속도 영향 주기
-            float distance = Vector2.Distance(rect_BackGround.position, rect_JoyStick.position) / radius;
-
             // 방향 벡터 구하기
             value = value.normalized;
 
             // 방향과 레버 거리를 delegate를 통해 전달
-            OnMoveInput?.Invoke(value, distance);  // 거리 값은 0 ~ 1 사이
+            OnMoveInput?.Invoke(value);  // 거리 값은 0 ~ 1 사이
 
         }
     }
