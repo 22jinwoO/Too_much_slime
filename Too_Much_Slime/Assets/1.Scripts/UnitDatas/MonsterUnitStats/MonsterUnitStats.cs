f54e2cb30310f726ba22a5750c7b42e4557ca638
22jinWoO <jinwoo2511@naver.com>
2024-09-13 00:03:24 +0900

    feat : 몬스터 정보에 자신의 팩토리 변수 추가
    몬스터 정보에 자신의 팩토리 변수 추가

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/MonsterUnitStats.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/MonsterUnitStats.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public abstract class MonsterUnitStats : BaseUnitStats
 {
+    public IMonsterFactory MonsterFactory;
+
     protected float dropValue;
 
     public abstract void InitData(int currentStage);
