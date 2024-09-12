c040c8b3acb9b0573b84e37631fdddbfb8d47cf7
22jinWoO <jinwoo2511@naver.com>
2024-09-12 17:50:54 +0900

    feat : 플레이어 이동 기능 구현
    플레이어 이동 기능 구현

---
+++ Too_Much_Slime/Assets/1.Scripts/UnitAct/PlayerMove.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour, IUnitDoAct
+{
+    [SerializeField] private Rigidbody2D rigid;
+    [SerializeField] private PlayerUnitStats stats;
+    [SerializeField] private JoyStick joyStick;
+
+    // 조이스틱으로부터 받은 이동 방향 (X축만)
+    private Vector2 movementDirection;
+
+    // 조이스틱 레버의 거리 비율 (0~1)
+    private float movementDistance;
+
+    // 자동으로 이동할 y축 속도
+    [SerializeField] private float autoMoveSpeedY = 2f;  // 원하는 속도로 설정 가능
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        stats = GetComponent<PlayerUnitStats>();
+    }
+
+    public void Enter()
+    {
+        joyStick.OnMoveInput += OnMovementInput;
+
+    }
+
+    public void DoAction()
+    {
+        // 조이스틱의 x축 방향으로만 이동
+        float moveX = movementDirection.x * movementDistance;
+
+        // y축은 자동으로 이동 (일정한 속도)
+        float moveY = autoMoveSpeedY;
+
+        // 플레이어의 이동
+        rigid.velocity = new Vector2(moveX, moveY) * stats.moveSpeed * Time.deltaTime;
+        print(rigid.velocity);
+    }
+
+    public void Exit()
+    {
+    }
+
+    // 이동 입력을 처리하는 delegate의 콜백 함수
+    private void OnMovementInput(Vector2 direction, float distance)
+    {
+        // 전달받은 조이스틱 입력에서 x축 값만 저장
+        movementDirection = new Vector2(direction.x, 0);  // y값은 무시하고 x값만 사용
+
+        // 레버 위치 비율 (0 ~ 1)
+        movementDistance = distance;
+
+
+        // 유저가 원하는 방향으로 이동하는 로직
+        // 입력 받기
+        float moveX = direction.x;
+
+        //bool isMove = moveX == 0;
+
+        //if (isMove) Exit();
+    }
+}
