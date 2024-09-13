0058b849f362c7042133341ecc5010faa74cf2fb
22jinWoO <jinwoo2511@naver.com>
2024-09-13 13:39:49 +0900

    fix : 플레이어 최고 높이값 프로퍼티 리턴값 변경
    플레이어 최고 높이값 프로퍼티 리턴값 변경

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/PlayerUnitStats/PlayerUnitStats.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/PlayerUnitStats/PlayerUnitStats.cs
@@ -7,7 +7,12 @@ public abstract class PlayerUnitStats : BaseUnitStats
     //플레이어 최고 높이값
     private float maxY
     {
-        get { return (transform.position.y); }
+        get 
+        {
+            if (transform.position.y < 0f) return 0f;
+            if (transform.position.y > 250f) return 250f;
+            return (transform.position.y); 
+        }
     } 
 
     // 읽기 전용으로만 쓸 때, 이 값을 불러와도 값에 변화를 주지 못하게 읽기로만 
