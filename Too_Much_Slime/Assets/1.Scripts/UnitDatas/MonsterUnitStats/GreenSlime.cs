268593da205a1e7158bf51e7afd3268c9fbf273f
22jinWoO <jinwoo2511@naver.com>
2024-09-13 13:39:23 +0900

    fix : 스테이지가 증가될 수록, 몬스터 스탯도 증가되도록 수정
    스테이지가 증가될 수록, 몬스터 스탯도 증가되도록 수정

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/GreenSlime.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/GreenSlime.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class GreenSlime : MonsterUnitStats
 {
+    private float standard_MaxHp;
+    private float standard_atkDmg;
     public override void InitData(int currentStage)
     {
         Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+        rigid.constraints = RigidbodyConstraints2D.FreezeAll;
         GetComponent<BoxCollider2D>().isTrigger = false;
 
         rigid.velocity = Vector2.zero;
         rigid.
         transform.rotation = Quaternion.Euler(Vector3.zero);
 
-        maxHp = 500 * currentStage;
+        // 오브젝트 타입이 몬스터 일 경우 - 스테이지 단계 N 당 몬스터 공격력, 체력 N * 10%씩 강화
+        maxHp = standard_MaxHp * (1 + currentStage * 0.1f);
         curHp = maxHp;
-        atkDmg = 5f * currentStage;
+        atkDmg = standard_atkDmg * (1 + currentStage * 0.1f);
         atkSpd = 0.3f;
     }
 
     private void Awake()
     {
+        standard_MaxHp = 100f;
+        standard_atkDmg = 3f;
+        maxHp = standard_MaxHp;
+        atkDmg = standard_atkDmg;
         InitData(1);
     }
 }
