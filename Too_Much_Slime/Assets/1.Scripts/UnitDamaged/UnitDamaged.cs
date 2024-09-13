43b952f3c833934ec3b2cb5a7e0f9c2ab0403bf3
22jinWoO <jinwoo2511@naver.com>
2024-09-13 13:38:57 +0900

    fix : Hp감소 조건 추가 및 몬스터 피격효과 구현
    Hp감소 조건 추가 및 몬스터 피격효과 구현

--- Too_Much_Slime/Assets/1.Scripts/UnitDamaged/UnitDamaged.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDamaged/UnitDamaged.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class UnitDamaged : MonoBehaviour
@@ -9,21 +10,42 @@ public class UnitDamaged : MonoBehaviour
     public Transform attacker;
 
     [SerializeField] private OnDeadEvent deadEvent;
+
+    [SerializeField] private SpriteRenderer sprite;
+
     private void Awake()
     {
         stats = GetComponent<BaseUnitStats>();
         deadEvent = GetComponent<OnDeadEvent>();
+
+        if(gameObject.CompareTag("Monster")) sprite =GetComponent<SpriteRenderer>();
     }
 
     public void Damaged(float atkDmg)
     {
+        if (stats.curHp <= 0f) return;
+
         stats.curHp -= atkDmg;
+
+        if (gameObject.CompareTag("Monster")) StartCoroutine(nameof(OnHit));
 
         if (gameObject.CompareTag("Monster") && stats.curHp <= 0)
         {
-            attacker.GetComponent<PlayerAttack>().target = null;
+            if (attacker != null) attacker.GetComponent<PlayerAttack>().target = null;
+
             deadEvent.OnDead(stats, attacker);
         }
         
+    }
+
+    IEnumerator OnHit()
+    {
+        sprite.color = new Color(0.9f, 0.2f, 0.2f);
+
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        sprite.color = new Color(1f, 1f, 1f);
+
+
     }
 }
