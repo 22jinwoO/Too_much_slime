14626531a46699111f85ca690d7d7436c07ec9e6
22jinWoO <jinwoo2511@naver.com>
2024-09-18 13:58:04 +0900

    feat : 플레이어 유닛 상점 매니저 기능 구현
    플레이어 유닛 상점 매니저 기능 구현

---
+++ Too_Much_Slime/Assets/1.Scripts/Managers/ShopManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    public Shop shop;
+
+    [SerializeField] private Button exitBtn;
+    [SerializeField] private Button reloadBtn;
+
+    private void Awake()
+    {
+        exitBtn.onClick.AddListener(ShopClose);
+        reloadBtn.onClick.AddListener(OnReload);
+    }
+
+    public void OnReload()
+    {
+        shop.SetSkillCards();
+        PlayerJamManager.Instance.playerJamCnt -= 2;
+        PlayerJamManager.Instance.jamCntTxt.text = PlayerJamManager.Instance.playerJamCnt.ToString();
+    }
+
+    public void ShopClose()
+    {
+        shop.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void ShopOpen()
+    {
+        shop.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
