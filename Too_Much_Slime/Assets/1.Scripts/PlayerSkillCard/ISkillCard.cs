f536ab5070cf7a2fa6b76847e4cd3ebc91a2785d
22jinWoO <jinwoo2511@naver.com>
2024-09-18 13:44:45 +0900

    feat : 스킬 카드 인터페이스 이미지 변수, 콘텐츠 밸류 추가 기능 구현
    스킬 카드 인터페이스 이미지 변수, 콘텐츠 밸류 추가 기능 구현

--- Too_Much_Slime/Assets/1.Scripts/PlayerSkillCard/ISkillCard.cs
+++ Too_Much_Slime/Assets/1.Scripts/PlayerSkillCard/ISkillCard.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public interface ISkillCard
 {
+    public Sprite AtrributeImg { get; set; }
+    public Sprite SKillImg { get; set; }
     public PlayerUnitStats Player { get; set; }
     public cardType Skill_Type { get; set; }
+    public string TitleValue { get; set; }
+
+    public string SkillContentValue { get; set; }
 
     public int CardLevel{ get; set;}
+    public int NeedJam { get; set;}
 
     public void SkillCard_LevelUp();
 }
