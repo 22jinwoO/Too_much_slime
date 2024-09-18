31a72d3647b646b2e070c1beb4a371afb261f9ad
22jinWoO <jinwoo2511@naver.com>
2024-09-18 14:15:03 +0900

    feat : 플레이어 유닛 초기 값 수정, HpBar 구현, 아이템 충돌 기능 구현
    플레이어 유닛 초기 값 수정, HpBar 구현, 아이템 충돌 기능 구현

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/PlayerUnitStats/Warrior.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/PlayerUnitStats/Warrior.cs
@@ -1,31 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Warrior : PlayerUnitStats
 {
+    [SerializeField] private ShopManager shopManager;
+
+    [SerializeField] private Image hpBar;
+    [SerializeField] private TextMeshProUGUI hpTxt;
+
     public override void InitData()
     {
+        transform.position = new Vector3(0, -5f, 0);
         maxHp = 500;
         curHp = maxHp;
         atkDmg = 80f;
         atkSpd = 0.8f;
-        moveSpeed = 100f;
+        moveSpeed = 150f;
+
+        anim.SetBool(hashDead, false);
+
+        twistingBlades.InitData();
+        followeBirds.InitData();
     }
 
     private void Awake()
     {
-        InitData();
         twistingBlades = GetComponent<TwistingBlades>();
         followeBirds = GetComponent<FollowBird>();
+
+        InitData();
+
+    }
+
+    private void Update()
+    {
+        hpBar.fillAmount = curHp / maxHp;
+        hpTxt.text = Mathf.RoundToInt(curHp).ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.transform.CompareTag("Skill_Item"))
+        switch (collision.transform.tag)
         {
-            collision.gameObject.GetComponent<ISkillCard>().SkillCard_LevelUp();
-        }
+            case "Jam":
+                int jamCnt = 1;
+                PlayerJamManager.Instance.GetJam(jamCnt);
+                Destroy(collision.gameObject);
+                break;
 
+            case "Skill_Item":
+                collision.gameObject.GetComponent<ISkillCard>().SkillCard_LevelUp();
+                break;
+
+            case "JamShop":
+                Destroy(collision.gameObject);
+                shopManager.ShopOpen();
+                break;
+
+            case "3Jams":
+                int jamsCnt = 3;
+                PlayerJamManager.Instance.GetJam(jamsCnt);
+                Destroy(collision.gameObject);
+                break;
+        }
     }
 }
