50f7d208cf075692a7679b8174cdd90d65793179
22jinWoO <jinwoo2511@naver.com>
2024-09-18 14:04:25 +0900

    feat : 킹 그린 슬라임 보스 구현
    킹 그린 슬라임 보스 구현

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/GreenSlimeKing.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/GreenSlimeKing.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class GreenSlime : MonsterUnitStats
+public class GreenSlimeKing : MonsterUnitStats
 {
-
     private float standard_MaxHp;
     private float standard_atkDmg;
+
+    [SerializeField] private Image hpBar;
+    [SerializeField] private TextMeshProUGUI hpTxt;
+
 
     private void Awake()
     {
@@ -17,14 +21,19 @@ public class GreenSlime : MonsterUnitStats
         monsterAttack = GetComponent<MonsterAttack>();
         unitDamaged = GetComponent<UnitDamaged>();
 
-        standard_MaxHp = 100f;
-        standard_atkDmg = 3f;
+        standard_MaxHp = 3000f;
+        standard_atkDmg = 15f;
         maxHp = standard_MaxHp;
         atkDmg = standard_atkDmg;
-        InitData(1);
-
+        InitData(StageManager.Instance.stageNum);
     }
 
+    private void Update()
+    {
+        hpBar.fillAmount = curHp / maxHp;
+        hpTxt.text = Mathf.RoundToInt(curHp).ToString();
+
+    }
     public override void InitData(int currentStage)
     {
         monsterAttack.target = null;
@@ -34,10 +43,10 @@ public class GreenSlime : MonsterUnitStats
 
         rigid.velocity = Vector2.zero;
 
-        spr.color = new Color(1f,1f,1f);
+        spr.color = new Color(1f, 1f, 1f);
         transform.rotation = Quaternion.Euler(Vector3.zero);
 
-        // 오브젝트 타입이 몬스터 일 경우 - 스테이지 단계 N 당 몬스터 공격력, 체력 N * 10%씩 강화
+        // 보스 몬스터 일 경우 - 스테이지 단계 N 당 몬스터 공격력, 체력 N * 10%씩 강화
         maxHp = standard_MaxHp * (1 + currentStage * 0.1f);
         curHp = maxHp;
         atkDmg = standard_atkDmg * (1 + currentStage * 0.1f);
