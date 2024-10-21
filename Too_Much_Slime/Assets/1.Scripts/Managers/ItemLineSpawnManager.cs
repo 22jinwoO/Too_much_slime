6cfb2d67e1d4fe520b65cc7c3349c5a7d99a236d
22jinWoO <jinwoo2511@naver.com>
2024-10-22 02:50:41 +0900

    fix : 아이템 라인 생성 구문 스타트 지점 변경
    아이템 라인 생성 구문 스타트 지점 변경

--- Too_Much_Slime/Assets/1.Scripts/Managers/ItemLineSpawnManager.cs
+++ Too_Much_Slime/Assets/1.Scripts/Managers/ItemLineSpawnManager.cs
@@ -10,8 +10,7 @@ public class ItemLineSpawnManager : MonoBehaviour
 
     public void CreateItemLine()
     {
-        print("함수 실행" + spawnManager.maxSpawnPoint);
-        for (int i = 0; i < spawnManager.maxSpawnPoint; i += 10)
+        for (int i = 10; i < spawnManager.maxSpawnPoint; i += 10)
         {
             GameObject prefab = Instantiate(shopLinePrefab, Vector3.zero + new Vector3(0, i, 0), Quaternion.identity);
             prefab.transform.SetParent(spawnManager.spawnser);
