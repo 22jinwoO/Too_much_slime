4a0e2a06cd17875c601bda4d5d1cd70b27a34eb6
22jinWoO <jinwoo2511@naver.com>
2024-10-22 03:10:05 +0900

    fix : 슬라임 초기값 할당 시 스테이지 넘버에 따라 초기값 증가되도록 수정
    슬라임 초기값 할당 시 스테이지 넘버에 따라 초기값 증가되도록 수정

--- Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/GreenSlime.cs
+++ Too_Much_Slime/Assets/1.Scripts/UnitDatas/MonsterUnitStats/GreenSlime.cs
@@ -21,12 +21,14 @@ public class GreenSlime : MonsterUnitStats
         standard_atkDmg = 3f;
         maxHp = standard_MaxHp;
         atkDmg = standard_atkDmg;
-        InitData(1);
+        InitData(StageManager.Instance.stageNum);
 
     }
 
     public override void InitData(int currentStage)
     {
+        boxCol.enabled = true;
+
         monsterAttack.target = null;
 
         rigid.constraints = RigidbodyConstraints2D.FreezeAll;
@@ -40,7 +42,7 @@ public class GreenSlime : MonsterUnitStats
         // 오브젝트 타입이 몬스터 일 경우 - 스테이지 단계 N 당 몬스터 공격력, 체력 N * 10%씩 강화
         maxHp = standard_MaxHp * (1 + currentStage * 0.1f);
         curHp = maxHp;
-        atkDmg = standard_atkDmg * (1 + currentStage * 0.1f);
+        atkDmg = standard_atkDmg * (1 + currentStage * 0.7f);
         atkSpd = 1.5f;
     }
 
