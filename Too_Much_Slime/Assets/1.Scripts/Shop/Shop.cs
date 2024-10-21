fd869792143bb9f1ec994b8f02e94deb97221330
22jinWoO <jinwoo2511@naver.com>
2024-10-22 03:06:09 +0900

    fix : 변수 선언 수정 및 주석 추가
    변수 선언 수정 및 주석 추가

--- Too_Much_Slime/Assets/1.Scripts/Shop/Shop.cs
+++ Too_Much_Slime/Assets/1.Scripts/Shop/Shop.cs
@@ -8,19 +8,17 @@ public class Shop : MonoBehaviour
 {
     [SerializeField] PlayerSkillManager playerSkillManager;
 
-    [SerializeField]
-    private Transform slotParent;
+    [SerializeField] private Transform slotParent;
 
-    [SerializeField]
-    private Slot[] slots;
+    [SerializeField] private Slot[] slots;
 
-    [SerializeField]
-    private Button slotBtn;
+    [SerializeField] private Button[] slotBtns;
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
         slots = slotParent.GetComponentsInChildren<Slot>();
+        slotBtns = slotParent.GetComponentsInChildren<Button>();
     }
 #endif
 
@@ -28,18 +26,33 @@ public class Shop : MonoBehaviour
     {
         SetSkillCards();
     }
+
     public void SetSkillCards()
     {
         for(int i = 0; i < slots.Length; i++)
         {
             int rand = Random.Range(0, playerSkillManager.skillCards.Length);
+
+            // 슬롯의 버튼 활성화
+            slotBtns[i].interactable = true;
+
+            // 슬롯의 랜덤한 스킬 카드 할당
             slots[i].skillCard = playerSkillManager.skillCards[rand];
+
+            // 스킬 카드 구매 시 필요한 잼 개수 표시
             slots[i].jamCntTxt.text = playerSkillManager.skillCards[rand].NeedJam.ToString();
+
+            // 스킬 카드 속성 이미지 표시
             slots[i].skillAttribute.sprite = playerSkillManager.skillCards[rand].AtrributeImg;
+
+            // 스킬 아이콘 표시
             slots[i].skillImg.sprite = playerSkillManager.skillCards[rand].SKillImg;
+
+            // 스킬 속성 카드 이름 텍스트 반영
             slots[i].titleTxt.text = playerSkillManager.skillCards[rand].TitleValue;
+
+            // 스킬 속성 카드 내용 텍스트 반영
             slots[i].contentTxt.text = playerSkillManager.skillCards[rand].SkillContentValue;
         }
     }
-
 }
